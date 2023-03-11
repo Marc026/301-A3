e9330d3eca653d59ed8d69cc4c918b92c79be8a6
Marc026 <mlapira@horizon.csueastbay.edu>
2023-03-10 17:29:46 -0800

    added comments.

--- Dictionary.cpp
+++ Dictionary.cpp
@@ -1,35 +1,35 @@
 #include "Dictionary.h"
 // Your code here
 
-class Node {
+class Node {                    //default constructor for Node
 public:
-    bool IsWord;
-    Node* children[NUM_CHARS];
+    bool IsWord;                //IsWord variable
+    Node* children[NUM_CHARS];  //Make NUMCHARs children
 
     Node() {
-        IsWord = false;
-        for (int i = 0; i < NUM_CHARS; i++) {
-            children[i] = nullptr;
+        IsWord = false;         //set the flag to false
+        for (int i = 0; i < NUM_CHARS; i++) {   //make new children nodes
+            children[i] = nullptr;      //set them to nullptr
         }
     }
 };
 
-Dictionary::Dictionary() {
-    root = new Node;
-    numWords = 0;
-    Node();
+Dictionary::Dictionary() {      //default constructor
+    root = new Node;            //root to new node
+    numWords = 0;               //0 words
+    Node();                     //call node constructor
 }
 
-Dictionary::Dictionary(const Dictionary &otherDict) {
-    root = new Node;
-    numWords = 0;
-    copyOther(otherDict);
+Dictionary::Dictionary(const Dictionary &otherDict) {   //2nd constructor
+    root = new Node;            //root to new node
+    numWords = 0;               //0 words
+    copyOther(otherDict);       //call copy0ther function with parameter otherDict
 }
 
-Dictionary::Dictionary(string filename) {
-    root = new Node;
-    numWords = 0;
-    LoadDictionaryFile(filename);
+Dictionary::Dictionary(string filename) {   //loads file into 2nd constructor
+    root = new Node;            //root to new node
+    numWords = 0;               //0 words
+    LoadDictionaryFile(filename);   //loads file
 }
 
 Dictionary &Dictionary::operator=(const Dictionary &otherDict) {
@@ -42,89 +42,88 @@ Dictionary &Dictionary::operator=(const Dictionary &otherDict) {
 }
 
 void Dictionary::LoadDictionaryFile(string filename) {
-    ifstream infile;
-    string content = "";
+    ifstream infile;          //ifstream variable/object
+    string content = "";      //empty string to input content into
     infile.open(filename); //Inserts each word into the dictionary
 
-    while(!infile.eof())
+    while(!infile.eof())     //while not at end of file
     {
-        getline(infile,content);
-        AddWord(content);
+        getline(infile,content);    //get the content in the file
+        AddWord(content);            //add the words into the data structure
     }
     infile.close();
 }
 
 void Dictionary::SaveDictionaryFile(string filename) {
-    ofstream outFile(filename);
-    if (!outFile.is_open()) {
+    ofstream outFile(filename);     //ofstream variable/object into filename
+    if (!outFile.is_open()) {         //if not open, throw error
         throw DictionaryError(filename + " failed to open");
     }
-    SaveDictionaryHelper(root, "", outFile);
+    SaveDictionaryHelper(root, "", outFile);    //save into file
 }
 
 void Dictionary::AddWord(string word) {
-    Node* node = root;
-    for (int i = 0; i < word.length(); i++) {
-        char c = word[i];
-        if (c < 'a' || c > 'z') {
-            throw DictionaryError("Invalid character");
+    Node* node = root;          //set node to root
+    for (int i = 0; i < word.length(); i++) {   //check how long each word is
+        char c = word[i];       //set one letter of the word to the index of the word
+        if (c < 'a' || c > 'z') {   //check if it is not a character
+            throw DictionaryError("Invalid character"); //throw error
         }
-        int index = c - 'a';
-        if (node->children[index] == nullptr) {
-            node->children[index] = new Node();
+        int index = c - 'a';    //find its position in the word
+        if (node->children[index] == nullptr) {     //if getting to the end of the word is null
+            node->children[index] = new Node();     //make a new node
         }
-        node = node->children[index];
+        node = node->children[index];               //set node to the length of the indexed word
     }
-    node->IsWord = true;
-    numWords++;
+    node->IsWord = true;         //set IsWord to true to say it is a word
+    numWords++;                 //bookkeeping
 }
 
-void Dictionary::MakeEmpty() {
-    destroyHelper(root);
-    // Rebuild root and reset number of words
-    root = new Node;
-    numWords = 0;
+void Dictionary::MakeEmpty() {  //clears data structure
+    destroyHelper(root);    //calls destroy function
+    root = new Node;        // Rebuild root
+    numWords = 0;           //and reset number of words
 }
 
 bool Dictionary::IsWord(string word) {
-    Node* node = root;
-    for (int i = 0; i < word.length(); i++) {
-        char c = word[i];
-        if (c < 'a' || c > 'z') {
-            throw DictionaryError("Invalid character");
+    Node* node = root;      //set node to root
+    for (int i = 0; i < word.length(); i++) {   //checks length of word
+        char c = word[i];       //sets one letter of the word to the index of the word
+        if (c < 'a' || c > 'z') {   //check if it is not a character
+            throw DictionaryError("Invalid character"); //throw error
         }
-        int index = c - 'a';
-        if (node->children[index] == nullptr) {
+        int index = c - 'a';    //find its position in the word
+        if (node->children[index] == nullptr) {     //if getting to the end of the word is null
             return false;
         }
-        node = node->children[index];
+        node = node->children[index];   //set node to the length of the indexed word
     }
-    return node->IsWord;
+    return node->IsWord;    //returns that it is a word
 }
 
 bool Dictionary::IsPrefix(string word) {
-    Node* node = root;
-    for (int i = 0; i < word.length(); i++) {
-        char c = word[i];
-        if (c < 'a' || c > 'z') {
-            throw DictionaryError("Invalid character");
+    Node* node = root;      //set node to root
+    for (int i = 0; i < word.length(); i++) {   //checks length of word
+        char c = word[i];   //sets one letter of the word to the index of the word
+        if (c < 'a' || c > 'z') {   //check if it is not a character
+            throw DictionaryError("Invalid character");     //throw error
         }
-        int index = c - 'a';
-        if (node->children[index] == nullptr) {
+        int index = c - 'a';    //find its position in the word
+        if (node->children[index] == nullptr) {     //if getting to the end of the word is null
             return false;
         }
-        node = node->children[index];
+        node = node->children[index];       //set node to the length of the indexed word
     }
-    return true;
-    //return (temp != nullptr && temp->IsPrefix == true);
+    return true;    //returns it is a prefix
+
 }
 
 int Dictionary::WordCount() {
-    return numWords;
+    return numWords;        //returns number of words
 }
 
 void Dictionary::copyOther(const Dictionary &otherDict) {
-    MakeEmpty();
+    MakeEmpty();    //calls make empty function
 
     // copy over the number of words from otherDict
     numWords = otherDict.numWords;
@@ -136,25 +135,25 @@ void Dictionary::copyOther(const Dictionary &otherDict) {
 }
 
 void Dictionary::destroyHelper(Dictionary::Node *thisTree) {
-    if (thisTree == nullptr){
-        return;
+    if (thisTree == nullptr){   //if tree already empty/nullptr
+        return;     //return
     }
 
-    destroyHelper(thisTree->left);
-    destroyHelper(thisTree->right);
-    delete thisTree;
+    destroyHelper(thisTree->left);      //destroy left tree
+    destroyHelper(thisTree->right);     //destroy right tree
+    delete thisTree;                            //delete
 }
 
 void Dictionary::copyHelper(Dictionary::Node *&thisTree, Dictionary::Node *otherTree) {
-    if (otherTree == nullptr) {
-        thisTree = nullptr;
+    if (otherTree == nullptr) {     //if tree already empty/nullptr
+        thisTree = nullptr;         //set thisTree to nullptr
         return;
     }
 
-    thisTree = new Node;
-    thisTree->IsWord = otherTree->IsWord;
+    thisTree = new Node;            //make new node
+    thisTree->IsWord = otherTree->IsWord;   //point this tree to other tree
 
-    for (int i = 0; i < NUM_CHARS; i++) {
+    for (int i = 0; i < NUM_CHARS; i++) {   //start copying over characters
         copyHelper(thisTree->children[i], otherTree->children[i]);
     }
 }
@@ -165,13 +164,13 @@ void Dictionary::SaveDictionaryHelper(Dictionary::Node *curr, string currPrefix,
         return;
     }
 
-    if (curr->IsWord) {
-        outFile << currPrefix << endl;
+    if (curr->IsWord) {     //if currently on a word
+        outFile << currPrefix << endl;      //cout
     }
 
-    for (int i = 0; i < NUM_CHARS; ++i) {
-        char letter = 'a' + i;
-        string next_prefix = currPrefix + letter;
+    for (int i = 0; i < NUM_CHARS; ++i) {       //for length of word
+        char letter = 'a' + i;      //find its indexed position
+        string next_prefix = currPrefix + letter;   //add letter
         SaveDictionaryHelper(curr->children[i], next_prefix, outFile);
     }
 }
