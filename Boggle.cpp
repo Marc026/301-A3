50bed0ad3321c58068a21bd1a2d5c37f74fc231b
Marc026 <mlapira@horizon.csueastbay.edu>
2023-03-20 20:30:26 -0700

    Added comments for Boggle.cpp.

--- Boggle.cpp
+++ Boggle.cpp
@@ -2,36 +2,34 @@
 #include "Boggle.h"
 using namespace std;
 
-
 // Your code here
-
 
 // This function is done.
 Dictionary Boggle::GetDictionary() {
-    return dict;
+    return dict;                    //dict Dictionary in Boggle
 }
 
 // This function is done.
 Dictionary Boggle::WordsFound() {
-    return wordsFound;
+    return wordsFound;              //wordsFound Dictionary in Boggle
 }
 
-Boggle::Boggle() {
-    dict.LoadDictionaryFile("dictionary.txt");
-    visited[BOARD_SIZE][BOARD_SIZE] = {0};
-    string board = " ";
+Boggle::Boggle() {                                      //default constructor
+    dict.LoadDictionaryFile("dictionary.txt");  //load dictionary file
+    visited[BOARD_SIZE][BOARD_SIZE] = {0};              //make a visit board all in 0
+    string board = " ";                                 //make board string
 }
 
-Boggle::Boggle(string filename) {
-    dict.LoadDictionaryFile(filename);
-    visited[BOARD_SIZE][BOARD_SIZE] = {0};
-    string board = " ";
+Boggle::Boggle(string filename) {                       //load file constructor
+    dict.LoadDictionaryFile(filename);                  //load file needed
+    visited[BOARD_SIZE][BOARD_SIZE] = {0};              //make a visit board all in 0
+    string board = " ";                                 //make board string
 }
 
 void Boggle::SetBoard(string board[BOARD_SIZE][BOARD_SIZE]) {
-    for (int row = 0; row < BOARD_SIZE; row++) {
+    for (int row = 0; row < BOARD_SIZE; row++) {        //set size of board
         for (int col = 0; col < BOARD_SIZE; col++) {
-            this->board[row][col] = board[row][col];
+            this->board[row][col] = board[row][col];    //deep copy of board
         }
     }
 }
@@ -39,14 +37,12 @@ void Boggle::SetBoard(string board[BOARD_SIZE][BOARD_SIZE]) {
 void Boggle::SolveBoard(bool printBoard, ostream &output) {
     wordsFound.MakeEmpty(); // reset wordsFound dictionary
 
-    for(int i=0; i<BOARD_SIZE; i++) {
+    for(int i=0; i<BOARD_SIZE; i++) {                   //checks every position of board
         for(int j=0; j<BOARD_SIZE; j++) {
-            // Reset visited array for each position on the board
-
             // Call SolveBoardHelper for current position on the board
-            int order[BOARD_SIZE][BOARD_SIZE] = {0};
-            int orderCounter = 0;
-            string prefix;
+            int order[BOARD_SIZE][BOARD_SIZE] = {0};    //order board of whats been visited
+            int orderCounter = 0;                       //set order of visited to 0
+            string prefix;                              //prefix
             int step = 0;
             SolveBoardHelper(i, j, prefix, 1, output, printBoard, orderCounter);
         }
@@ -54,26 +50,23 @@ void Boggle::SolveBoard(bool printBoard, ostream &output) {
 }
 
 void Boggle::SaveSolve(string filename) {
-    wordsFound.SaveDictionaryFile(filename);
+    wordsFound.SaveDictionaryFile(filename);        //save output to filename
 }
 
-void Boggle::PrintBoard(ostream &output, string prefix) {
-
+void Boggle::PrintBoard(ostream &output, string prefix) { //prints board to output
     for (int i = 0; i < BOARD_SIZE; i++) {
         for (int j = 0; j < BOARD_SIZE; j++) {
-            if (visited[i][j] > 0) {
-                output << " '" << board[i][j] << "' " ;
+            if (visited[i][j] > 0) {            //if position has been visited
+                output << " '" << board[i][j] << "' " ;     //put quotes around letter
             } else {
-                output << "  " << board[i][j] << "  ";
+                output << "  " << board[i][j] << "  ";  //else just put spaces
             }
         }
-        output << "\t";
-        for(int k = 0; k < BOARD_SIZE; k++){
-            output << "  " << visited[i][k] << "  " ;
-        }
-
+        output << "\t";     //tab
+        for(int k = 0; k < BOARD_SIZE; k++){    //same as previous board but instead
+            output << "  " << visited[i][k] << "  " ;   //a board of 0 and
+        }                                       //incremented visited positions
         output << endl;
-
     }
     output << endl;
 }
@@ -98,15 +91,15 @@ void Boggle::SolveBoardHelper(int row, int col, string prefix, int step, ostream
         return;
     }
 
-// Mark current position as visited
+    //increment in which it has visited that position
     orderCounter++;
+    // Mark current position as visited
     visited[row][col] = orderCounter;
 
 // Check if prefix is a valid word
     if (dict.IsWord(prefix)) {
         // Check if word is already found
         if (!wordsFound.IsWord(prefix)) {
-
             // Add word to wordsFound dictionary
             wordsFound.AddWord(prefix);
 
@@ -114,9 +107,9 @@ void Boggle::SolveBoardHelper(int row, int col, string prefix, int step, ostream
             if(wordsFound.WordCount() > 0 && printBoard != true){
                 output << wordsFound.WordCount() << "\t" << prefix << endl;
             }
-            if (printBoard == true) {
-                output << "Word: " << prefix << endl;
-                output << "Number of Words: " << wordsFound.WordCount() << endl;
+            if (printBoard == true) {   //if user wants to display board
+                output << "Word: " << prefix << endl;   //output the info
+                output << "Number of Words: " << wordsFound.WordCount() << endl;    //output info
                 PrintBoard(output, prefix);
             }
         }
